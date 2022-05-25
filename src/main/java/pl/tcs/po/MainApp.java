580c69bcc2778ea98179b220d1395c718800e66a
Kuba <jakub8.kowalczyk@student.uj.edu.pl>
2022-05-26 01:31:49 +0200

    added main menu window

--- src/main/java/pl/tcs/po/MainApp.java
+++ src/main/java/pl/tcs/po/MainApp.java
@@ -5,18 +5,23 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import pl.tcs.po.controllers.MainMenuController;
 import pl.tcs.po.controllers.MainWindowController;
 
 import java.io.IOException;
 import java.util.Objects;
-import java.util.PrimitiveIterator;
 
 public class MainApp extends Application {
 
+    public static MainApp instance;
+
+    Stage mainStage;
+
     @Override
     public void start(Stage stage) throws IOException {
-
-        Parent root = getMainWindow();
+        instance = this;
+        Parent root = getMainMenuWindow();
+        mainStage = stage;
 
         Scene scene = new Scene(root);
         stage.setScene(scene);
@@ -34,5 +39,16 @@ public class MainApp extends Application {
     private Parent getMainWindow() throws IOException {
         var url = MainWindowController.class.getClassLoader().getResource("MainWindow.fxml");
         return FXMLLoader.load(Objects.requireNonNull(url));
+    }
+
+    private Parent getMainMenuWindow() throws IOException {
+        var url = MainMenuController.class.getClassLoader().getResource("MainMenu.fxml");
+        return FXMLLoader.load(Objects.requireNonNull(url));
+    }
+
+    public void runNewSandbox() throws IOException {
+        Parent root = getMainWindow();
+        Scene scene = new Scene(root);
+        mainStage.setScene(scene);
     }
 }
---
+++ src/main/java/pl/tcs/po/controllers/MainMenuController.java
@@ -0,0 +1,96 @@
+package pl.tcs.po.controllers;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import pl.tcs.po.MainApp;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainMenuController implements Initializable {
+
+    @FXML
+    StackPane stackPane;
+
+    ImageView backgroundImageView, logoImageView;
+    Button newButton, loadButton, settingsButton;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        initializeImageViews();
+        initializeButtons();
+        Group group = getMenuGroup();
+        stackPane.getChildren().add(group);
+    }
+
+    private Group getMenuGroup() {
+        VBox vBox = new VBox();
+        HBox hBox = new HBox();
+        hBox.getChildren().addAll(
+                createSpacer(), newButton, createSpacer(), loadButton, createSpacer(), settingsButton, createSpacer()
+        );
+        vBox.setPrefHeight(stackPane.getPrefHeight());
+        vBox.getChildren().addAll(logoImageView, createSpacer(), hBox, createSpacer(),createSpacer());
+
+        return new Group(backgroundImageView, vBox);
+    }
+
+    /**
+     * returns a spacer used to space elements in VBox or HBox evenly
+     */
+    private Node createSpacer() {
+        final Region spacer = new Region();
+        HBox.setHgrow(spacer, Priority.ALWAYS);
+        VBox.setVgrow(spacer, Priority.ALWAYS);
+        return spacer;
+    }
+
+    private void initializeButtons() {
+        Image newImage = new Image(getClass().getResource("/images/new.png").toString());
+        ImageView newImageView = new ImageView(newImage);
+        Image loadImage = new Image(getClass().getResource("/images/load.png").toString());
+        ImageView loadImageView = new ImageView(loadImage);
+        Image settingsImage = new Image(getClass().getResource("/images/settings.png").toString());
+        ImageView settingsImageView = new ImageView(settingsImage);
+
+        newButton = new Button();
+        newButton.setGraphic(newImageView);
+        newButton.setStyle("-fx-background-color: #5d69bf;");
+        newButton.setOnAction(e -> {
+            try {
+                MainApp.instance.runNewSandbox();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        });
+        loadButton = new Button();
+        loadButton.setGraphic(loadImageView);
+        loadButton.setStyle("-fx-background-color: #5d69bf; ");
+        loadButton.setOnAction(e -> {
+            // TODO load file
+        });
+        settingsButton = new Button();
+        settingsButton.setGraphic(settingsImageView);
+        settingsButton.setStyle("-fx-background-color: #5d69bf; ");
+        settingsButton.setOnAction(e -> {
+            // TODO go to settings
+        });
+
+
+    }
+
+    private void initializeImageViews() {
+        Image backgroundImage = new Image(getClass().getResource("/images/menuBackground.png").toString());
+        Image logoImage = new Image(getClass().getResource("/images/logo.png").toString());
+        backgroundImageView = new ImageView(backgroundImage);
+        logoImageView = new ImageView(logoImage);
+    }
+}
